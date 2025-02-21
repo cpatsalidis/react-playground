e6c3c5a9270dd93f34a4caf6f29b23271983e0cd
cpatsalidis <christos.patsalidis20@imperial.ac.uk>
2025-02-21 12:00:00 +0200

    final log

--- tic-tac-toe/src/components/Log.jsx
+++ tic-tac-toe/src/components/Log.jsx
@@ -2,7 +2,7 @@ function Log({ currentLog }) {
     return (
         <ol id="log">
             {currentLog.map((info, index) => {
-                return <li className="highlighted" key={index}>{info.square.col}</li>
+                return <li className={!index ? "highlighted" : ''} key={index}>{info.active}: ({info.square.row}x{info.square.col})</li>
             })}
         </ol>
     )
