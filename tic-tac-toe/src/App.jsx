e35de792fe0596119697f1d22db8c890973c66ef
cpatsalidis <christos.patsalidis20@imperial.ac.uk>
2025-02-22 12:00:00 +0200

    simple winnner

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import Player from './components/Player'
-import GameBoard from './components/GameBoard'
 import Log from './components/Log'
+import GameBoard from './components/GameBoard'
+import { WINNING_COMBINATIONS } from './WinningCominations'
 
+let initialGameBoard = Array.from({ length: 3 }, () => Array.from({ length: 3 }, () => ({ symbol: '', isPressed: false })))
 
 function activePlayerHelper(log) {
   let player = 'X'
@@ -14,15 +16,32 @@ function activePlayerHelper(log) {
 
 function App() {
   let [log, setLog] = useState([]);
-
+  let gameBoard = initialGameBoard
   let activePlayer = activePlayerHelper(log)
+
+  for (let turn of log) {
+    let { square, active, isPressed } = turn
+    let { row, col } = square
+
+    gameBoard[row][col].symbol = active
+    gameBoard[row][col].isPressed = isPressed
+  }
+
+  let winner = null;
+
+  for (let combination of WINNING_COMBINATIONS) {
+    let firstSymbol = gameBoard[combination[0].row][combination[0].column].symbol
+    let secondSymbol = gameBoard[combination[1].row][combination[1].column].symbol
+    let thirdSymbol = gameBoard[combination[2].row][combination[2].column].symbol
+
+    if (firstSymbol && firstSymbol === secondSymbol && firstSymbol === thirdSymbol) {
+      winner = firstSymbol
+    }
+  }
 
   function handleTurn(rowIndex, colIndex) {
     setLog(prevLog => {
       let player = activePlayerHelper(prevLog)
-
-      if (prevLog.length > 0 && prevLog[0].active === 'X') player = 'O'
-
       let newLog = [{ square: { row: rowIndex, col: colIndex }, active: player, isPressed: true }, ...prevLog]
 
       return newLog
@@ -38,7 +57,8 @@ function App() {
           <Player initialName="Player 2" symbol="O" active={activePlayer === 'O'} />
         </ol>
 
-        <GameBoard onSelectSquare={handleTurn} turns={log} />
+        {winner && <p>{winner} is the Winner!!</p>}
+        <GameBoard onSelectSquare={handleTurn} board={gameBoard} />
       </div>
       <Log currentLog={log} />
     </main>
--- tic-tac-toe/src/components/GameBoard.jsx
+++ tic-tac-toe/src/components/GameBoard.jsx
@@ -1,18 +1,7 @@
-function GameBoard({ onSelectSquare, turns }) {
-    let initialGameBoard = Array.from({ length: 3 }, () => Array.from({ length: 3 }, () => ({ symbol: '', isPressed: false })))
-    let GameBoard = initialGameBoard
-
-    for (let turn of turns) {
-        let { square, active, isPressed } = turn
-        let { row, col } = square
-
-        GameBoard[row][col].symbol = active
-        GameBoard[row][col].isPressed = isPressed
-    }
-
+function GameBoard({ onSelectSquare, board }) {
     return (
         <ol id="game-board">
-            {GameBoard.map((row, rowIndex) => <li key={rowIndex}>
+            {board.map((row, rowIndex) => <li key={rowIndex}>
                 <ol>
                     {row.map((buttonInfo, colIndex) => <li key={colIndex}><button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={buttonInfo.isPressed}>{buttonInfo.symbol}</button></li>)}
                 </ol>
